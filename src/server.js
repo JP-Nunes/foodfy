cfb5a567814eb226e9f2a6d610b3f295584b9e32
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-04-23 17:52:28 -0300

    Using nunjucks to send backend data to frontend

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
-server.set('view engine', 'html')
+server.set('view engine', 'njk')
 
 server.use(express.static('public'))
 server.use(express.static('src/assets'))
