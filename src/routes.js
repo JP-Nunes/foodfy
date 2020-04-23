c26c93ce61acfa5c1c588c1d6fa8432e524dde83
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-04-23 17:53:10 -0300

    Rendering pages with data from data.js

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const data = require('./data') 
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-   return res.render('index')
+   const recipes = [...data]
+   const recipeIndex = req.params.index
+
+   return res.render('index', { recipes, recipeIndex })
 })
 
 routes.get('/recipes/:index', (req, res) => {
-   return res.render('recipes/show')
+   const recipes = [...data]
+   const recipeIndex = req.params.index
+   const recipe = recipes[recipeIndex]
+
+   return res.render('recipes/show', { recipe })
 })
 
 module.exports = routes
