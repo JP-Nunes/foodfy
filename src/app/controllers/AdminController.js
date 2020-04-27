93936af62d4979feea79e824036cd9ccfa598a20
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-04-27 15:19:32 -0300

    Starting the implementation of new backend functions

---
+++ src/app/controllers/AdminController.js
@@ -0,0 +1,23 @@
+module.exports = {
+   index(req, res) {
+      res.send('to be implemented')
+   },
+   create(req, res) {
+      res.send('to be implemented')
+   },
+   show(req, res) {
+      res.send('to be implemented')
+   },
+   edit(req, res) {
+      res.send('to be implemented')
+   },
+   post(req, res) {
+      res.send('to be implemented')
+   },
+   put(req, res) {
+      res.send('to be implemented')
+   },
+   delete(req, res) {
+      res.send('to be implemented')
+   }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,28 @@ const express = require('express')
 const data = require('./data') 
 const routes = express.Router()
 
+const AdminController = require('./app/controllers/AdminController')
+
 routes.get('/', (req, res) => {
    const recipes = [...data]
 
    return res.render('index', { recipes })
 })
-
-routes.get('/recipes/:index', (req, res) => {
+routes.get('/recipes/:id', (req, res) => {
    const recipes = [...data]
    const recipeIndex = req.params.index
    const recipe = recipes[recipeIndex]
 
    return res.render('recipes/show', { recipe })
 })
+
+routes.get('/admin/recipes', AdminController.index)
+routes.get('/admin/recipes/create', AdminController.create)
+routes.get('/admin/recipes/:id', AdminController.show)
+routes.get('/admin/recipes/:id/edit', AdminController.edit)
+
+routes.post('/admin/recipes', AdminController.post)
+routes.put('/admin/recipes', AdminController.put)
+routes.delete('/admin/recipes', AdminController.delete)
 
 module.exports = routes
