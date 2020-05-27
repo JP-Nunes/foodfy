74120be6df0813f6e4b2596e0a61463bfa8c66af
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-05-27 18:23:26 -0300

    Frontend paination logic

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,40 @@ for(item of menuItems) {
       item.classList.add('active')
    }
 }
+
+// Paginação
+
+function paginate(totalPages, selectedPage) {
+   
+   let pages = []
+   let oldPage
+
+   for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+      const firstAndLastPages = currentPage == 1 || currentPage == totalPages
+      const pagesAfterCurrentPage = currentPage <= selectedPage + 2
+      const pagesBeforeCurrentPage = currentPage >= selectedPage - 2
+      
+      if(firstAndLastPages || pagesAfterCurrentPage && pagesBeforeCurrentPage) {
+         
+         if(oldPage && currentPage - oldPage > 2) {
+            pages.push('...')
+         }
+         if(oldPage && currentPage - oldPage == 2) {
+            pages.push(oldPage + 1)
+         }
+         
+         pages.push(currentPage)
+
+         oldPage = currentPage
+      }
+      
+   }
+   return pages 
+}
+
+const pagination = document.querySelector('.pagination')
+const page = +pagination.dataset.page
+const total = +pagination.dataset.total
+
+const pages = paginate(total ,page)
