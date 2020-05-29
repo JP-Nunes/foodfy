4c4e78f438860560d52908394855a800be0ece83
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-05-29 20:50:53 -0300

    Query to create tables files and recipe_files

--- database.sql
+++ database.sql
@@ -14,10 +14,22 @@ CREATE TABLE "recipes" (
 
 CREATE TABLE "chefs" (
   "id" SERIAL PRIMARY KEY,
-  "name" text NOT NULL,
+  "name" text NOT NULL,""
   "image" text NOT NULL,
   "created_at" timestamp DEFAULT (now())
 );
+
+CREATE TABLE files (
+	"id" SERIAL PRIMARY KEY,
+  "name" text,
+  "path" text NOT NULL
+)
+
+CREATE TABLE recipe_files (
+	"id" SERIAL PRIMARY KEY,
+  "recipe_id" INTEGER REFERENCES recipes(id),
+  "file_id" INTEGER REFERENCES files(id)
+)
 
 -- Joining tables
 
