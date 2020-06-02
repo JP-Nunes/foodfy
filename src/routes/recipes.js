0e42c4253b57532120377a0406c382c1b0446795
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-01 23:53:09 -0300

    Using multer on router

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = express.Router()
 
+const multer = require('../app/middlewares/multer')
+
 const RecipeController = require('../app/controllers/RecipeController')
 
 routes.get('/', RecipeController.index)
@@ -8,8 +10,8 @@ routes.get('/create', RecipeController.create)
 routes.get('/:id', RecipeController.show)
 routes.get('/:id/edit', RecipeController.edit)
 
-routes.post('/', RecipeController.post)
-routes.put('/', RecipeController.put)
+routes.post('/', multer.array('images', 5), RecipeController.post)
+routes.put('/', multer.array('images', 5), RecipeController.put)
 routes.delete('/', RecipeController.delete)
 
 module.exports = routes
