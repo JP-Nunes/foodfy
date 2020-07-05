05ed3bdcd2e3b477da19963b861e1aa27f9ddc66
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-05 12:27:20 -0300

    Using loaders and validator

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,53 +1,31 @@
+const FileLoader = require('../services/LoadFilesService')
+const RecipeLoader = require('../services/LoadRecipesService')
+const ChefsLoader = require('../services/LoadChefsService')
+
 const Recipe = require('../models/Recipe')
 const Chef = require('../models/Chef')
 const File = require('../models/File')
 
 module.exports = {
    async index(req, res) {
-      let recipes = await Recipe.allRecipesWithChefsNames()
+      try {
+         let recipes = await Recipe.allRecipesWithChefsNames()
       
-      const recipesWithFilePromise = recipes.map(async recipe => {
-         const recipeFiles = await File.allRecipeFiles(recipe.id)
-         const firstFile = recipeFiles[0]
+         recipes = await FileLoader.loadRecipesFiles(recipes)
 
-         return recipe = {
-            ...recipe,
-            image_src: `${req.protocol}://${req.headers.host}${firstFile.path.replace('public', '')}`
-         }
-      })
-      recipes = await Promise.all(recipesWithFilePromise)
-
-      return res.render('home/index',  { recipes })
+         return res.render('home/index',  { recipes })
+      } catch (error) {
+         console.error(error)
+      }
    },
    async recipes(req, res) {
       try {
-         let { page } = req.query
+         const { page } = req.query
 
-         page = page || 1
-         const limit = 6
-         let offset = (page - 1) * limit
-
-         const params = {
-            limit, offset
-         }
-         
-         let recipes = await Recipe.paginate(params)
-
-         const recipesWithFilePromise = recipes.map(async recipe => {
-            const files = await File.allRecipeFiles(recipe.id)
-            const firstFile = files[0]
-
-            return recipe = {
-               ...recipe,
-               image_src: `${req.protocol}://${req.headers.host}${firstFile.path.replace('public', '')}`
-            }
-         })
-         recipes = await Promise.all(recipesWithFilePromise)
-
-         const pagination = {
-            page,
-            total: Math.ceil(recipes[0].total / limit)
-         }
+         const { 
+            recipes, 
+            pagination 
+         } = await RecipeLoader.loadPaginatedRecipes(page)
 
          return res.render('home/recipes', { recipes, pagination })
 
@@ -57,19 +35,7 @@ module.exports = {
    },
    async chefs(req, res) {
       try {
-         let chefs = await Chef.finAllAndCountRecipes()
-         
-         const chefsWithFilesPromise = chefs.map(async chef => {
-            const file = await File.findOne({ 
-               where: { id: chef.file_id}
-            })
-
-            return chef = {
-               ...chef,
-               image_src: `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`
-            }
-         })
-         chefs = await Promise.all(chefsWithFilesPromise)
+         const chefs = await ChefsLoader.loadChefs()
          
          return res.render('home/chefs', { chefs })
       } catch (error) {
@@ -78,28 +44,12 @@ module.exports = {
    },
    async searchRecipes(req, res) {
       try {
-         const { filter } = req.query
+         const { filter } = req
 
-         if(filter) {
-            let recipes = await Recipe.filtered(filter)
+         const recipes = 
+            await RecipeLoader.loadFilteredRecipes(filter)
 
-            const recipesWithFilePromise = recipes.map(async recipe => {
-               const files = await File.allRecipeFiles(recipe.id)
-               const firstFile = files[0]
-   
-               return recipe = {
-                  ...recipe,
-                  image_src: `${req.protocol}://${req.headers.host}${firstFile.path.replace('public', '')}`
-               }
-            })
-            recipes = await Promise.all(recipesWithFilePromise)
-
-            return res.render('home/search', { recipes, filter })
-         } else {
-            const recipes = await Recipe.findAll()
-            
-            return res.render('home/search', { recipes })
-         }    
+         return res.render('home/search', { recipes, filter })
       } catch (error) {
          console.error(error)
       }
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const routes = express.Router()
 
+const Validator = require('../app/validators/home')
+
 const HomeController = require('../app/controllers/HomeController')
 
 routes.get('/index', HomeController.index)
 routes.get('/recipes', HomeController.recipes)
 routes.get('/chefs', HomeController.chefs)
-routes.get('/search', HomeController.searchRecipes)
+routes.get('/search', Validator.searchRecipes, HomeController.searchRecipes)
 
 module.exports = routes
