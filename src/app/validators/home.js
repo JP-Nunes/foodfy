35540c825d2f05f57ac4e001fb49cd1821634da2
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-06 23:41:24 -0300

    Validating if there are recipes

--- src/app/validators/home.js
+++ src/app/validators/home.js
@@ -3,6 +3,12 @@ const Recipe = require('../models/Recipe')
 
 module.exports = {
    async searchRecipes(req, res, next) {
+      const recipes = await Recipe.findAll()
+      
+      if(recipes.length === 0) {
+         return res.render('home/search')
+      }
+
       const { filter } = req.query
 
       if(!filter) {
@@ -18,5 +24,17 @@ module.exports = {
       req.filter = filter
 
       next()
+   },
+   async recipes(req, res, next) {
+      const recipes = await Recipe.findAll()
+      
+      if(recipes.length === 0) {
+         return res.render('home/recipes')
+      }
+
+      next()
+   },
+   async recipes(req, res, next) {
+      
    }
 }
--- src/routes/home.js
+++ src/routes/home.js
@@ -6,7 +6,7 @@ const Validator = require('../app/validators/home')
 const HomeController = require('../app/controllers/HomeController')
 
 routes.get('/index', HomeController.index)
-routes.get('/recipes', HomeController.recipes)
+routes.get('/recipes', Validator.recipes, HomeController.recipes)
 routes.get('/chefs', HomeController.chefs)
 routes.get('/search', Validator.searchRecipes, HomeController.searchRecipes)
 
