b09c6453ee02613ed0aac247c5af86b512c10424
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-08 22:40:07 -0300

    Organizing Highlight and paginate scripts

--- public/scripts.js
+++ public/scripts.js
@@ -186,82 +186,99 @@ const Validate = {
    }
 }
 
+const Paginate = {
+   pagination: document.querySelector('.pagination'),
+   createPages() {
+      if(this.pagination) {
+         const page = this.pagination.dataset.page
+         const total = this.pagination.dataset.total
 
+         const pages = this.paginate(
+            total, 
+            page
+         )
 
-
-
-
-// Header menu
-
-const currentPage = location.href
-const menuItems = document.querySelectorAll('header .menu ul li a')
-
-for(item of menuItems) {
-   if(currentPage.includes(item.getAttribute('href'))) {
-      item.classList.add('active')
-   }
-}
-
-// Paginação
-
-function paginate(totalPages, selectedPage) {
-   
-   let pages = []
-   let oldPage
-
-   for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
-
-      const firstAndLastPages = currentPage == 1 || currentPage == totalPages
-      const pagesAfterCurrentPage = currentPage <= selectedPage + 2
-      const pagesBeforeCurrentPage = currentPage >= selectedPage - 2
-      
-      if(firstAndLastPages || pagesAfterCurrentPage && pagesBeforeCurrentPage) {
+         let element = ''
          
-         if(oldPage && currentPage - oldPage > 2) {
-            pages.push('...')
-         }
-         if(oldPage && currentPage - oldPage == 2) {
-            pages.push(oldPage + 1)
+         for(let page of pages) {
+            if(String(page).includes('...')) {
+               element += `<span>${page}</span>`
+            } else if(page == 1) {
+               element += `
+                  <a 
+                     href="?page=${page}" 
+                     class="active" 
+                     id="first-item"
+                  >
+                  ${page}
+                  </a>
+               `
+            } else {
+               element += `
+                  <a 
+                     href="?page=${page}"
+                     onclick="Highlight.highlightPagination()"
+                  >
+                  ${page}
+                  </a>`
+            }
          }
          
-         pages.push(currentPage)
-
-         oldPage = currentPage
+         this.pagination.innerHTML = element
       }
-   }
-   return pages 
+   },
+   paginate(totalPages, selectedPage) {
+      let pages = []
+      let oldPage
+   
+      for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+   
+         const firstAndLastPages = currentPage == 1 || currentPage == totalPages
+         const pagesAfterCurrentPage = currentPage <= selectedPage + 2
+         const pagesBeforeCurrentPage = currentPage >= selectedPage - 2
+         
+         if(firstAndLastPages || pagesAfterCurrentPage && pagesBeforeCurrentPage) {
+            
+            if(oldPage && currentPage - oldPage > 2) {
+               pages.push('...')
+            }
+            if(oldPage && currentPage - oldPage == 2) {
+               pages.push(oldPage + 1)
+            }
+            
+            pages.push(currentPage)
+   
+            oldPage = currentPage
+         }
+      }
+      return pages 
+   },
+
 }
 
-const pagination = document.querySelector('.pagination')
-const page = +pagination.dataset.page
-const total = +pagination.dataset.total
-
-const pages = paginate(total ,page)
-
-let element = ''
-
-for(let page of pages) {
-   if(String(page).includes('...')) {
-      element += `<span>${page}</span>`
-   } else if(page == 1) {
-      element += `<a href="?page=${page}" class="active" id="first-item">${page}</a>`
-   } 
-   else {
-      element += `<a href="?page=${page}">${page}</a>`
-   }
-}
-
-pagination.innerHTML = element
-
-const currentUrl = location.href
-const paginationItems = document.querySelectorAll('.pagination a')
-const firstItem = document.getElementById('first-item')
-
-for(item of paginationItems) {
-   if(currentUrl.includes(item.getAttribute('href')) && 
-      item.getAttribute('href') !== firstItem.getAttribute('href')) {
+const Highlight = {
+   highlightHeader() {
+      const currentPage = location.href
+      const menuItems = document.querySelectorAll('header .menu ul li a')
       
-      item.classList.add('active')
-      firstItem.classList.remove('active')
+      for(item of menuItems) {
+         if(currentPage.includes(item.getAttribute('href'))) {
+            item.classList.add('active')
+         }
+      }
+   },
+   highlightPagination() {
+      const currentUrl = location.href
+      const paginationItems = document.querySelectorAll('.pagination a')
+      const firstItem = document.getElementById('first-item')
+      
+      for(item of paginationItems) {
+         if(currentUrl.includes(item.getAttribute('href')) && 
+            item.getAttribute('href') !== firstItem.getAttribute('href')) {
+            
+            item.classList.add('active')
+            firstItem.classList.remove('active')
+         }
+      }
    }
 }
