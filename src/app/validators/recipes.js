477e1166f807a76a3c7ca2b406d0aab793a65881
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-11 22:49:02 -0300

    Validating steps and ingredients

--- src/app/validators/recipes.js
+++ src/app/validators/recipes.js
@@ -3,17 +3,52 @@ const FileLoader = require('../services/LoadFilesService')
 
 const Recipe = require("../models/Recipe")
 const Chef = require("../models/Chef")
-const { user } = require('../controllers/RecipeController')
+
+function format(recipe) {
+   const filteredPreparation = 
+      recipe.preparation.filter(step => {
+         return step != ''
+      })
+   
+      
+   const filteredIngredients = 
+      recipe.ingredients.filter(ingredient => {
+         return ingredient != ''
+      })
+      
+   recipe = {
+      ...recipe,
+      ingredients: filteredIngredients,
+      preparation: filteredPreparation
+   }
+
+   return recipe
+}
 
 module.exports = {
+   async index(req, res, next) {
+      const { page } = req.query
+
+      const { 
+         recipes, 
+         pagination 
+      } = await RecipeLoader.loadPaginatedRecipes(page)
+
+      if(!recipes) {
+         return res.render('recipes/index')
+      }
+
+      req.recipes = recipes
+      req.pagination = pagination
+
+      next()
+   },
    async show(req, res, next) {
       try {
          const {
             recipe,
             recipeFiles
-         } = await RecipeLoader.loadRecipe({
-            where: { id: req.params.id }
-         })
+         } = await RecipeLoader.loadRecipe(req.params.id)
 
          if(!recipe) {
             return res.render('recipes/index', {
@@ -58,7 +93,7 @@ module.exports = {
                })
             }
          }
-
+         
          if(req.files.length < 1) {
             return res.render('recipes/create', {
                error: 'É necessário ao menos uma imagem',
@@ -66,6 +101,19 @@ module.exports = {
                chefsNameAndId
             })
          }
+         
+         const formattedRecipe = format(req.body)
+         
+         if(!formattedRecipe.preparation[0] ||
+            !formattedRecipe.ingredients[0]) {
+            return res.render('recipes/create', {
+               error: 'Todos os campos são necessários',
+               recipe: req.body,
+               chefsNameAndId
+            }) 
+         }
+
+         req.newRecipe = formattedRecipe
 
          next()
       } catch (error) {
@@ -75,7 +123,6 @@ module.exports = {
    },
    async put(req, res, next) {
       try {
-         const { id } = req.body
          let { removed_files } = req.body
          const chefsNameAndId = await Chef.nameAndId()
          
@@ -90,14 +137,15 @@ module.exports = {
                })
             }
          }
+
+         const recipeFiles = 
+            await FileLoader.loadRecipeFiles(req.body.id)        
          
-         const recipeFiles = await FileLoader.loadRecipeFiles(id)         
-   
          if(removed_files) {
             removed_files = removed_files.split(',')
             const lastIndex = removed_files.length - 1
             removed_files.splice(lastIndex, 1)
-   
+            
             if(req.files == 0 && recipeFiles.length == removed_files.length) {
                return res.render(`recipes/edit`, {
                   error: 'É necessário ao menos uma imagem',
@@ -106,9 +154,20 @@ module.exports = {
                })
             }
          }
+         
+         const formattedRecipe = format(req.body)
 
-         req.recipeFiles = recipeFiles
-   
+         if(!formattedRecipe.preparation[0] ||
+            !formattedRecipe.ingredients[0]) {
+            return res.render(`recipes/edit`, {
+               error: 'Todos os campos são necessários',
+               recipe: req.body,
+               chefsNameAndId
+            }) 
+         }
+         
+         req.recipe = formattedRecipe
+
          next()   
       } catch (error) {
          console.error(error)  
